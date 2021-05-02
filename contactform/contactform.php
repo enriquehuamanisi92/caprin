cee5e2a280dfa81fc0351212c659d8f638fdcd75
enriquehuamanisi92 <enriquehuamanisi@gmail.com>
2021-05-01 22:18:20 -0500

    Envio de corre por php
    ejecuta el proceso de php para el envio de correo

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,13 +1,22 @@
 <?php
 
-// the message
-$msg = "First line of text\nSecond line of text";
-
-// use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
-
-// send email
-mail("enriquehuamanisi@gmail.com","My subject",$msg);
+    $nombre = $_POST['name'];
+    $email = $_POST['email'];
+    $asunto = $_POST['subject'];
+    $mensaje = $_POST['message'];
 
 
+    $to      = 'enriquehuamanisi@gmail.com';
+    $subject =  $asunto;
+    $message =  'Nombre y Apellido:'.$nombre. "\r\n" .
+                'Email:'.$email. "\r\n" .
+                'Asunto: '.$email. "\r\n" .
+                'Asunto: '.$mensaje. "\r\n";
+
+                
+    $headers = 'From: enriquehuamanisi@gmail.com'       . "\r\n" .
+                 'Reply-To: enriquehuamanisi@gmail.com' . "\r\n" .
+                 'X-Mailer: PHP/' . phpversion();
+
+    mail($to, $subject, $message, $headers);
 ?>
