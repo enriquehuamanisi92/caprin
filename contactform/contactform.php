269f897d8a1c39e4201b35979032da307124ce78
enriquehuamanisi92 <enriquehuamanisi@gmail.com>
2021-05-01 22:58:21 -0500

    Update contactform.php

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,22 +1,13 @@
 <?php
-
     $nombre = $_POST['name'];
     $email = $_POST['email'];
     $asunto = $_POST['subject'];
     $mensaje = $_POST['message'];
 
-
-    $to      = 'enriquehuamanisi@gmail.com';
-    $subject =  $asunto;
-    $message =  'Nombre y Apellido:'.$nombre. "\r\n" .
-                'Email:'.$email. "\r\n" .
-                'Asunto: '.$email. "\r\n" .
-                'Asunto: '.$mensaje. "\r\n";
-
-                
-    $headers = 'From: enriquehuamanisi@gmail.com'       . "\r\n" .
-                 'Reply-To: enriquehuamanisi@gmail.com' . "\r\n" .
-                 'X-Mailer: PHP/' . phpversion();
-
+    $to = $email;
+    $subject = $asunto ;
+    $message = $mensaje;
+    $headers = "From: enriquehuamanisi@gmail.com" . "\r\n" . "CC: enriquehuamanisi@gmail.com";
+    
     mail($to, $subject, $message, $headers);
 ?>
