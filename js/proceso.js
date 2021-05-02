29f685a69ee1b5e862c1a262095972f5a475b453
enriquehuamanisi92 <enriquehuamanisi@gmail.com>
2021-05-01 22:48:34 -0500

    Este proceso ya no va !

--- js/proceso.js
+++ js/proceso.js
@@ -4,16 +4,16 @@
 
 function procesarDatos () {
 
-    alert("proceso")
+    //alert("proceso")
 
-    $.ajax({
-        type:"POST", // la variable type guarda el tipo de la peticion GET,POST,..
-        url:"php/proceso.php", //url guarda la ruta hacia donde se hace la peticion
-        data:{nombre:"pepe",edad:10}, // data recive un objeto con la informacion que se enviara al servidor
-        success:function(datos){ //success es una funcion que se utiliza si el servidor retorna informacion
-             console.log(datos.promedio)
-         },
-        dataType: dataType // El tipo de datos esperados del servidor. Valor predeterminado: Intelligent Guess (xml, json, script, text, html).
-    })
+    //$.ajax({
+      //  type:"POST", // la variable type guarda el tipo de la peticion GET,POST,..
+       // url:"php/proceso.php", //url guarda la ruta hacia donde se hace la peticion
+       // data:{nombre:"pepe",edad:10}, // data recive un objeto con la informacion que se enviara al servidor
+        //success:function(datos){ //success es una funcion que se utiliza si el servidor retorna informacion
+          //   console.log(datos.promedio)
+         //},
+        //dataType: dataType // El tipo de datos esperados del servidor. Valor predeterminado: Intelligent Guess (xml, json, script, text, html).
+    //})
 }
 
